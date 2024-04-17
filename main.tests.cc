ff1ae08cca5beb9e470eddc45221a776ef2ddf6c
Павел Михайлов <pasha8165@gmail.com>
2024-04-17 17:04:58 +0300

    added some tests

--- main.tests.cc
+++ main.tests.cc
@@ -5,13 +5,33 @@
 #define DROGON_TEST_MAIN
 #include <drogon/drogon_test.h>
 
-TEST(fetching, fetching_getJson_nonErrorJson)
+
+class ApiCallTest : public testing::Test {
+protected:
+	ApiCallTest()
+		: weather_json(fetchWeatherDataAsJson())
+	{}
+
+	const Json::Value weather_json;
+};
+
+TEST_F(ApiCallTest, fetching_getJson_jsonNotNullNoErrors)
 {
-	Json::Value json = fetchWeatherDataAsJson();
-	Json::Value error_code = json["error"]["code"];
+	Json::Value error_code = weather_json["error"]["code"];
 
 	ASSERT_TRUE(error_code.isNull()) << "error code is "<<error_code.asInt() <<"check https://www.weatherapi.com/docs/ for more info";
-	ASSERT_FALSE(json.isNull()) << "something went horribly wrong";
+	ASSERT_FALSE(weather_json.isNull()) << "something went horribly wrong";
+}
+
+TEST_F(ApiCallTest, fetching_getCurrentConditionIconURL_iconURLNotNull) {
+	Json::Value icon_url= weather_json["current"]["condition"]["icon"];
+
+	ASSERT_FALSE(icon_url.isNull());
+}
+
+TEST_F(ApiCallTest, fetching_getTommorowsForecast_tommorowsForecastNotNull) {
+	Json::Value tommorows_forecast = weather_json["forecast"]["forecastday"][1];
+	ASSERT_FALSE(tommorows_forecast.isNull());
 }
 
 
